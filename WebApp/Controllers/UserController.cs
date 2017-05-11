f9a95492d7e3b44c712f546889833f2a1537ba35
Gaoxin_Huang <gaoxin.huang@gmail.com>
2017-05-12 10:36:52 +1200

    finish database design and init web

---
+++ WebApp/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Model;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            using (DatabaseContext db = new DatabaseContext())
+            {
+                var userList = db.User.ToList().Select(u => new UserViewModel()
+                {
+                    Id = u.Id,
+                    Username = u.Username,
+                    Password = u.Password,
+                    Name = u.Name,
+                });
+                return View(userList);
+            }
+
+        }
+    }
+}
