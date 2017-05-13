3bb9c7c44c6a3128e4a14b9450f6e5dc56a961d3
Gaoxin_Huang <gaoxin.huang@gmail.com>
2017-05-13 18:37:02 +1200

    custom table, foreign key name, column name

--- Model/AccountModel.cs
+++ Model/AccountModel.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Model
 {
+    [Table("Account")]
     public class Account
     {
         public int Id { get; set; }
-        public string AccountName { get; set; }
+        [Column("AccountName")]
+        public string Accountname { get; set; }
+
         public int UserId { get; set; }
+        [ForeignKey("UserId")]
         public User User { get; set; }
     }
 }
--- Model/UserModel.cs
+++ Model/UserModel.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Model
 {
+    [Table("User")]
     public class User
     {
         public int Id { get; set; }
+        
         public string Username { get; set; }
         public string Password { get; set; }
         public string Name { get; set; }
